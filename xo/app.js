aab1bbe8fa92f1b73d95bb3472905b61eb650106
Oleg <margin2call@mail.ru>
2016-08-25 01:29:24 +0300

    xo - games POST endpoint

--- xo/app.js
+++ xo/app.js
@@ -9,11 +9,11 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-let games = [];
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('games', games);
+app.set('games', []);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
